f93128a5571f423c72185a08bdab23260b9f7c49
Kamil Grabowski <kamil.grabowski89x@gmail.com>
2018-09-06 18:48:08 +0200

    first commit

--- src/main/java/WriteObjectToFile/Main.java
+++ src/main/java/WriteObjectToFile/Main.java
@@ -1,8 +1,62 @@
 package WriteObjectToFile;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        List <Weather> weatherList = new ArrayList <Weather>();
+
+        Weather weather = new Weather(
+                "Bydgoszcz",
+                "www",
+                40,
+                60,
+                "Slonecznie",
+                54.54,
+                21.23
+        );
+
+        Weather weather2 = new Weather(
+                "Torun",
+                "www",
+                0,
+                0,
+                "Leje",
+                0.0,
+                0.0
+        );
+
+        Weather weather3 = new Weather(
+                "Gdansk",
+                "www",
+                30,
+                50,
+                "Deszcz",
+                20.20,
+                20.20
+        );
+        weatherList.add(weather);
+        weatherList.add(weather2);
+        weatherList.add(weather3);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        File filename = new File("weather.json");
+        try {
+            objectMapper.writeValue(filename, weatherList);
+
+        } catch (IOException e) {
+
+
+        }
+
+
 
     }
 }
---
+++ src/main/java/WriteObjectToFile/Weather.java
@@ -0,0 +1,80 @@
+package WriteObjectToFile;
+
+public class Weather {
+
+    private String city;
+    private String iconUrl;
+    private double temperature;
+    private double feelslikeC;
+    private String conditionText;
+    private double lat;
+    private double lon;
+
+    public Weather(String city, String iconUrl, double temperature, double feelslikeC, String conditionText, double lat, double lon) {
+        this.city = city;
+        this.iconUrl = iconUrl;
+        this.temperature = temperature;
+        this.feelslikeC = feelslikeC;
+        this.conditionText = conditionText;
+        this.lat = lat;
+        this.lon = lon;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getIconUrl() {
+        return iconUrl;
+    }
+
+    public void setIconUrl(String iconUrl) {
+        this.iconUrl = iconUrl;
+    }
+
+    public double getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(double temperature) {
+        this.temperature = temperature;
+    }
+
+    public double getFeelslikeC() {
+        return feelslikeC;
+    }
+
+    public void setFeelslikeC(double feelslikeC) {
+        this.feelslikeC = feelslikeC;
+    }
+
+    public String getConditionText() {
+        return conditionText;
+    }
+
+    public void setConditionText(String conditionText) {
+        this.conditionText = conditionText;
+    }
+
+    public double getLat() {
+        return lat;
+    }
+
+    public void setLat(double lat) {
+        this.lat = lat;
+    }
+
+    public double getLon() {
+        return lon;
+    }
+
+    public void setLon(double lon) {
+        this.lon = lon;
+    }
+
+
+}
